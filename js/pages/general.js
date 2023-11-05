c459b8fc1452f17a8d2894f13eebf8929259574f
Valen <valentinakeila12@gmail.com>
2023-11-04 21:23:01 -0300

    Co-authored-by: NicolasCataldi <NicolasCataldi@users.noreply.github.com>

---
+++ js/pages/general.js
@@ -0,0 +1,28 @@
+const tipoEleccion = 2 ; 
+const tipoRecuento = 2; 
+
+const periodoSelect = document.getElementById('periodoSelect');
+const cargoSelect = document.getElementById('cargoSelect');
+
+fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); 
+  })
+  .catch(error => {
+    console.error('Ocurrió un error:', error);
+  }); // devuelve los años ajajajaj me fije en consola y cada año tiene un value numerico calculo que con eso se identifica el select 
+  
+
+ 
+  
+
+
+      
+  
+
+
+
+
+
+ 
