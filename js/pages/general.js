3b32bca021c7e6fe132092209c13f3d2722e4bca
Valen <valentinakeila12@gmail.com>
2023-11-08 14:40:02 -0300

    commit

--- js/pages/general.js
+++ js/pages/general.js
@@ -1,8 +1,10 @@
 const tipoEleccion = 2 ; 
-const tipoRecuento = 2; 
+const tipoRecuento = 1; 
+
 
 const periodoSelect = document.getElementById('periodoSelect');
 const cargoSelect = document.getElementById('cargoSelect');
+const distritoSelect = document.getElementById('distritoSelect');
 
 fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
   .then(response => response.json())
@@ -11,7 +13,12 @@ fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
   })
   .catch(error => {
     console.error('Ocurrió un error:', error);
-  }); // devuelve los años ajajajaj me fije en consola y cada año tiene un value numerico calculo que con eso se identifica el select 
+  });
+   // devuelve los años ajajajaj me fije en consola y cada año tiene un value numerico calculo que con eso se identifica el select */
+
+  
+   
+
   
 
  
